7dbf4de42a45ee5263ff63ff33efabb89cc9fef5
TuxChad <141134233+TuxChad@users.noreply.github.com>
2023-08-03 13:33:58 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger")
+const nav_menu = document.querySelector(".nav-menu")
+
+hamburger.addEventListener("click",()=>{
+  hamburger.classList.toggle("active");
+  nav_menu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  nav_menu.classList.remove("active");
+}))
